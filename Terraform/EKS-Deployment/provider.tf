810b0856f5f535e2ad99f85a4b76c3196055baaf
krishnasyam <134209944+krishnasyam@users.noreply.github.com>
2025-04-15 16:01:34 +0530

    Update provider.tf

--- Terraform/EKS-Deployment/provider.tf
+++ Terraform/EKS-Deployment/provider.tf
@@ -1,9 +1,9 @@
 locals {
 
-  region          = "us-east-1"
+  region          = "ap-southeast-1"
   name            = "bankapp-eks-cluster"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
@@ -15,6 +15,6 @@ locals {
 
 provider "aws" {
 
-  region = "us-east-1"
+  region = "ap-southeast-1"
 
 }
